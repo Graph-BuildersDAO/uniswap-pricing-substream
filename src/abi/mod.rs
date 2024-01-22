1e8545f4a595b48b8e09116a6fabd7d12232f626
jmulq <james.mulqueeny@outlook.com>
2024-01-22 20:58:09 +0000

    feat: generated pb/abi files

---
+++ src/abi/mod.rs
@@ -0,0 +1,3 @@
+pub mod erc20;
+pub mod factory;
+pub mod pair;
---
+++ src/pb/mod.rs
@@ -0,0 +1,15 @@
+// @generated
+pub mod chainlink {
+    // @@protoc_insertion_point(attribute:chainlink.v1)
+    pub mod v1 {
+        include!("chainlink.v1.rs");
+        // @@protoc_insertion_point(chainlink.v1)
+    }
+}
+pub mod uniswap_pricing {
+    // @@protoc_insertion_point(attribute:uniswap_pricing.v1)
+    pub mod v1 {
+        include!("uniswap_pricing.v1.rs");
+        // @@protoc_insertion_point(uniswap_pricing.v1)
+    }
+}
